184415d4e655c675378c66b2af0161e13db94ae3
Lubomir Bucek <lubomir.bucek@eox.at>
2019-08-22 12:02:11 +0200

    WIP

---
+++ src/postbuild.sh
@@ -0,0 +1,1 @@
+rm dist/glyphicons-halflings-regular*
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,15 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/code-de.html',
+      minify: false,
+      inject: 'head',
+    }),
+  ]
 });
