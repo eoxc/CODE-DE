fccc1ea79fd26247e3b397d73adffbfb7c3fac84
Lubomir Bucek <lubomir.bucek@eox.at>
2019-11-20 15:50:46 +0100

    remove html plugin, uglify plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,39 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/template.html',
-      minify: false,
-      chunksSortMode: 'manual',
-      // sorting order necessary for CSS overwrites not having enough !default
-      chunks: ['vendors~code-de', 'vendorslarge~code-de', 'eoxdeps~code-de', 'eoxc~code-de', 'code-de']
-    }),
   ],
   devtool: 'source-map',
   optimization: {
     moduleIds: 'hashed',
-    minimizer: [
-      new UglifyJSPlugin({
-        sourceMap: true,
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          compress: {
-            passes: 2,
-            drop_console: true,
-          }
-        }
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ],
     runtimeChunk: false,
     splitChunks: {
       maxInitialRequests: Infinity,
